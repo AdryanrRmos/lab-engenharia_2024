84508979fe3f5ae0a0074c3aacc0b507656ce529
AdryanrRmos <84917581+AdryanrRmos@users.noreply.github.com>
2024-05-23 10:51:24 -0300

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -5,22 +5,25 @@ import './Home.css';
 const Home = () => {
   return (
     <div>
-      <h1>Tarefas em React  😄</h1>
+      <h1>Tarefas em React</h1>
       <nav>
         <ul>  
           <li>
-            <Link to={"/Atividade01"}>Atividade 1 - Letreiro e Relogio</Link>
+            <Link to="/Atividade01">Atividade 1 - Letreiro e Relogio</Link>
           </li>
           <li>
-            <Link to={"/Atividade02"}>Atividade 2 - Contador Homem e Mulher</Link>
+            <Link to="/Atividade02">Atividade 2 - Contador Homem e Mulher</Link>
           </li>
           <li>
-            <Link to={"/Atividade03"}>Atividade 3 - Componentes diversos</Link>
+            <Link to="/Atividade03">Atividade 3 - Componentes diversos</Link>
           </li>
+         </ul>
+      </nav>
       <footer>
             © 2024 | Desenvolvido por Adryan Ramos
       </footer>
     </div>
   );
 }
+
 export default Home;
